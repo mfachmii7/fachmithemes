747df760a0844a2c397bbf5088706f7c1bf5e14a
mfachmii7 <30945129+mfachmii7@users.noreply.github.com>
2017-08-12 11:04:17 +0700

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.fachmithemes.tk/") {
+        window.location.href = "http://www.fachmithemes.tk"; }});
+//]]>
